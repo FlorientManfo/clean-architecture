8db41a0e9cc2861feff1568c4e2f41b0c2f68f04
Florient MANFO <florientmanfo3@outlook.com>
2023-12-29 21:03:53 +0100

    Cleanup

--- app/src/main/java/com/example/cleanArchitecture/MainActivity.kt
+++ app/src/main/java/com/example/cleanArchitecture/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cleanArchitecture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,8 +14,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cleanArchitecture.ui.theme.CleanArchiTheme
 import com.example.domain.utils.NetworkService
@@ -55,10 +58,15 @@ fun NetworkMonitoring(modifier: Modifier = Modifier) {
         }
     })
 
-    Text(
-        text = amIOnline,
-        modifier = modifier
-    )
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = amIOnline,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Preview(showBackground = true)

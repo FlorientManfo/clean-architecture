9bb55afa0eca357b5c4682c78f56dbf8cac1f75b
Florient MANFO <florientmanfo3@outlook.com>
2024-01-09 22:12:46 +0100

    Cleanup and add component preview

--- wonderUI/src/main/java/com/wonder/wonderui/components/WonderSingleLineTextFiled.kt
+++ wonderUI/src/main/java/com/wonder/wonderui/components/WonderSingleLineTextFiled.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
